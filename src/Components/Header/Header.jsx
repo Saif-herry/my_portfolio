b0a866ed0acb7ebb89c2922d779d65edf9d8896a
Mohammad Saif <saif@Mohammads-MacBook-Air.local>
2025-08-11 13:50:46 +0530

    make Header fixed at the top while scrolling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,23 +3,58 @@ import { Navbar } from "../Navbar/Navbar";
 import { ThemeContext } from "../../Context/theme";
 import logo from "../../assets/logo.png";
 
-import "./Header.css";
 export const Header = () => {
   const [{ themename }] = React.useContext(ThemeContext);
 
   return (
     <>
-      <header className={"header center " + themename}>
-        <h3>
-          <a href="#home" className="link">
-            <span>
-              <img src={logo} alt="" />{" "}
-            </span>
-            <span>Mohammad Saif</span>
-          </a>
-        </h3>
-        <Navbar />
+      <header
+        style={{
+          position: "fixed", // now fixed at the top
+          top: 0, // stick to very top
+          left: 0,
+          right: 0,
+          zIndex: 999, // above all other content
+          backgroundColor: "inherit", // match theme background
+          width: "100%", // full width
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 20,
+        }}
+      >
+        <div
+          className={"header center " + themename}
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            gap: 40,
+            width: "100%",
+          }}
+        >
+          <h3>
+            <a
+              href="#home"
+              className="link"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <span>
+                <img
+                  src={logo}
+                  alt=""
+                  style={{ width: "30px", marginRight: "4px" }}
+                />
+              </span>
+              <span>Mohammad Saif</span>
+            </a>
+          </h3>
+          <Navbar />
+        </div>
       </header>
+
+      {/* Push page content down so it's not hidden behind the fixed header */}
+      <div style={{ height: "7em" }} />
     </>
   );
 };
