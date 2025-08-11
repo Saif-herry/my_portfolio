461ddc8bf558f14a5ffb09e73e2cc3bf4f0bcbed
Mohammad Saif <saif@Mohammads-MacBook-Air.local>
2025-08-11 13:57:52 +0530

    update resume link on navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,105 +1,105 @@
-import React from 'react'
-import { ThemeContext } from '../../Context/theme'
-import './Navbar.css'
+import React from "react";
+import { ThemeContext } from "../../Context/theme";
+import "./Navbar.css";
 
-import Brightness2Icon from '@mui/icons-material/Brightness2'
-import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded'
-import MenuIcon from '@mui/icons-material/Menu'
-import CloseIcon from '@mui/icons-material/Close'
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import WbSunnyRoundedIcon from "@mui/icons-material/WbSunnyRounded";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const Navbar = () => {
-  const [{ themename, toggeltheme }] = React.useContext(ThemeContext)
-  const [showNavList, setShowNavList] = React.useState(false)
+  const [{ themename, toggeltheme }] = React.useContext(ThemeContext);
+  const [showNavList, setShowNavList] = React.useState(false);
 
-  const toggleNavList = id => {
-    var element = document.getElementById(id)
+  const toggleNavList = (id) => {
+    var element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView()
+      element.scrollIntoView();
     }
-    setShowNavList(!showNavList)
-  } ///
+    setShowNavList(!showNavList);
+  }; ///
   return (
     <>
-      <nav className='center nav'>
+      <nav className="center nav">
         <ul
-          style={{ display: showNavList ? 'flex' : null }}
-          className='nav__list'
+          style={{ display: showNavList ? "flex" : null }}
+          className="nav__list"
         >
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='#home'
-              onClick={() => toggleNavList('#home')}
-              className='link link--nav'
+              href="#home"
+              onClick={() => toggleNavList("#home")}
+              className="link link--nav"
             >
               Home
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='#about'
-              onClick={() => toggleNavList('#about')}
-              className='link link--nav'
+              href="#about"
+              onClick={() => toggleNavList("#about")}
+              className="link link--nav"
             >
               About
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='#skills'
-              onClick={() => toggleNavList('#skills')}
-              className='link link--nav'
+              href="#skills"
+              onClick={() => toggleNavList("#skills")}
+              className="link link--nav"
             >
               Skills
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='#projects'
-              onClick={() => toggleNavList('#projects')}
-              className='link link--nav'
+              href="#projects"
+              onClick={() => toggleNavList("#projects")}
+              className="link link--nav"
             >
               Projects
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='#contact'
-              onClick={() => toggleNavList('#contact')}
-              className='link link--nav'
+              href="#contact"
+              onClick={() => toggleNavList("#contact")}
+              className="link link--nav"
             >
               Contact
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className="nav__list-item">
             <a
-              href='https://drive.google.com/file/d/1J-tRYPz92KFMszyK-c2fWIDuGwI91ddy/view'
+              href="https://drive.google.com/file/d/1f6EPMhafzoxsUQBIqjtH6EVNu9h7nkbw/view?usp=sharing"
               onClick={toggleNavList}
-              className='link link--nav'
-              target='_blank'
-              rel='noreferrer'
+              className="link link--nav"
+              target="_blank"
+              rel="noreferrer"
             >
               Resume
             </a>
           </li>
         </ul>
         <button
-          type='button'
+          type="button"
           onClick={toggeltheme}
-          className='btn btn--icon nav__theme'
-          aria-label='toggle theme'
-          style={{ backgroundColor: 'inherit' }}
+          className="btn btn--icon nav__theme"
+          aria-label="toggle theme"
+          style={{ backgroundColor: "inherit" }}
         >
-          {themename === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
+          {themename === "dark" ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
         </button>
         <button
-          type='button'
+          type="button"
           onClick={toggleNavList}
-          className='btn btn--icon nav__hamburger'
-          aria-label='toggle navigation'
+          className="btn btn--icon nav__hamburger"
+          aria-label="toggle navigation"
         >
           {showNavList ? <CloseIcon /> : <MenuIcon />}
         </button>
       </nav>
     </>
-  )
-}
+  );
+};
